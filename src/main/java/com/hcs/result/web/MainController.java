9e4ed2e35cfcc3c270bac14df02b55ea98f00a98
hcs <hcs@172.27.11.9>
2016-10-11 17:58:18 +0800

    添加MongoDB

--- src/main/java/com/hcs/result/web/MainController.java
+++ src/main/java/com/hcs/result/web/MainController.java
@@ -3,15 +3,23 @@ package com.hcs.result.web;
 import java.util.Date;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hcs.result.bean.Customer;
 
 @Controller
 public class MainController {
 	
 	@Value("${application.message:Hello World}")
 	private String message = "Hello World";
+	
+	@Autowired
+	private MongoTemplate mongoTemplate;
 
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
@@ -24,5 +32,19 @@ public class MainController {
 	public String foo(Map<String, Object> model) {
 		throw new RuntimeException("Foo");
 	}
+	
+	@RequestMapping("/customer")
+	@ResponseBody	
+	public String customer() {
+		Customer tracker = new Customer();
+		tracker.setUsername("test");
+		tracker.setId(1L);
+		tracker.setAge(11);
+		this.mongoTemplate.insert(tracker);
+		
+		return tracker.toString();
+	}
+	
+	
    }
 
